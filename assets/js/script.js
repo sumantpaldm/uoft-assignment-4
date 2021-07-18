05ce3c96decd877d8f876beadc4673be53046f29
SINGH 1258 <sumantpaldm@gmail.com>
2021-07-18 19:40:06 -0400

    final script

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ let containerEl = document.querySelector("#container");
 let headerEl = document.querySelector(".header");
 let ScoreEl = document.querySelector(".score");
 let ContentEl = document.querySelector("#content");
-let welcomeEl = document.querySelector("#welcome-screen");
+let initialEl = document.querySelector("#initial-screen");
 let questionsEl = document.querySelector("#questions");
 let choicesEl = document.querySelector("#choices");
 let answerEl = document.querySelector("#answer");
@@ -12,7 +12,7 @@ let scoreListEl = document.querySelector("#scoreList");
 let timerEl = document.querySelector("#timer");
 let isQuizDone = true;
 
-const questionsObj = [
+const quizData = [
     {
         question: "Commonly used data types DO NOT include",
         choices: { 1: "alerts", 2: "booleans", 3: "numbers", 4: "strings" },
@@ -40,7 +40,7 @@ const questionsObj = [
     },
     {
         question:
-            "String variables must be enclosed within _____ when being assigned to variables.",
+            "String variables must be enclosed within _____ when being assigned to variables",
         choices: {
             1: "commas",
             2: "quotes",
@@ -61,14 +61,14 @@ submitScore.textContent = "Submit";
 
 
 
-displayMainScreen();
+display();
 
-function displayMainScreen() {
-    let welcomeScreen = document.createElement("h1");
-    welcomeScreen.textContent = "Coding Quiz Challenge";
+function display() {
+    let initialScreen = document.createElement("h1");
+    initialScreen.textContent = "Coding Quiz Challenge";
 
-    let descriptionScreen = document.createElement("p");
-    descriptionScreen.textContent = `Try to answer the following code-related questions within the time
+    let paraScreen = document.createElement("p");
+    paraScreen.textContent = `Try to answer the following code-related questions within the time
   limit. Keep in mind that incorrect answers will penalize your score
   time by ten seconds!`;
 
@@ -81,21 +81,21 @@ function displayMainScreen() {
     startBtn.addEventListener("click", startTimer);
     startBtn.addEventListener("click", displayQuestions);
 
-    welcomeEl.appendChild(welcomeScreen);
-    welcomeEl.appendChild(descriptionScreen);
-    welcomeEl.appendChild(startBtn);
+    initialEl.appendChild(initialScreen);
+    initialEl.appendChild(paraScreen);
+    initialEl.appendChild(startBtn);
 }
 
-// Declaration of Questions, Choices and Answers
 
 
-const lastQuestion = questionsObj.length - 1;
+
+const lastQuestion = quizData.length - 1;
 let questionsCtr = 0;
 let totalTime = 100;
 let score = 100;
 let timer;
 
-// This function starts the timer countdown
+
 function startTimer() {
     timer = setInterval(() => {
         isQuizDone = false;
@@ -105,47 +105,49 @@ function startTimer() {
         score = totalTime;
         if (totalTime <= 0) {
             isQuizDone = true;
+            showScore(score);
             score = 0;
             clearInterval(timer);
-            showScore(score);
+
         }
     }, 1000);
 }
 
-// This function displays all the questions
+
 function displayQuestions() {
 
-    // variable to store the Questions output
+
     const output = [];
 
-    // variable to store the list of choices
+
     const choices = [];
 
-    let question = questionsObj[questionsCtr];
+    let question = quizData[questionsCtr];
 
     for (choice in question.choices) {
         choices.push(
             `<div>
-          <button name="question${questionsCtr}" onclick='checkAnswer(${choice})'>${choice} ${question.choices[choice]}</button>           
+          <button name="question${questionsCtr}" onclick='checkAnswer(${choice})'>${choice} ${question.choices[choice]}</button>
+          <br>
+          <br>
         </div>
         `
         );
     }
-    // add this question and its answers to the output
+
     output.push(
-        `   <div id="questions"> ${question.question} </div>
+        `   <div id="questions"><h2> ${question.question} </h2></div>
         <div id="choices"> ${choices.join("")} </div>
       `
     );
 
-    // finally combine our output list into one string of HTML and put it on the page
+
     ContentEl.innerHTML = output.join("");
 }
 
-/* This functions checks for answer if correct or wrong and
- check if there are still questions needs to be answered */
+
 function checkAnswer(answer) {
-    if (answer === parseInt(questionsObj[questionsCtr].answer)) {
+    if (answer === parseInt(quizData[questionsCtr].answer)) {
         displayResult("CORRECT");
     } else {
         displayResult("WRONG");
@@ -170,7 +172,7 @@ function checkAnswer(answer) {
     }
 }
 
-// This function shows a text if the answer is Correct or Wrong
+
 function displayResult(ans) {
 
     answerEl.innerHTML = `<div><hr><p>${ans}</p></div>`;
@@ -194,7 +196,7 @@ function showScore(score) {
     scoreResult.textContent = `Your final score is: ${score}`;
 
     let nameLabel = document.createElement("span");
-    nameLabel.textContent = "Enter your initials: ";
+    nameLabel.textContent = "Enter your NAME: ";
 
     let nameInput = document.createElement("input");
     nameInput.setAttribute("type", "text");
@@ -209,28 +211,26 @@ function showScore(score) {
     containerEl.appendChild(resultEl);
 }
 
-// Function to save the score to local storage
+
 function saveScore() {
     let initials = document.getElementById("nameId").value;
 
-    if (!initials) {
-        initials = "No initials"
-    }
+
     let existingData = localStorage.getItem("scores");
-    //console.log(JSON.parse(existingData));
+
     let existing = existingData ? JSON.parse(existingData) : [];
     let newData = { score: score, name: initials };
 
-    // Add new data to localStorage Array
+
     existing.push(newData);
 
-    // Save back to localStorage
+
     localStorage.setItem("scores", JSON.stringify(existing));
-    //getScores();
+
     displayScores();
 }
 
-//Function to show all the Scores
+
 function displayScores() {
 
     resultEl.style.display = "none";
@@ -241,14 +241,14 @@ function displayScores() {
     let tempList = [];
     let tempListEl = document.createElement("div");
 
-    // Declaration of Go Back Button
+
     let goBack = document.createElement("button");
     goBack.textContent = "Go Back";
     goBack.className = "btn edit-btn";
     goBack.setAttribute("id", "goback");
     goBack.setAttribute("click", "goBackToHome");
 
-    // Declaration of Clear Score Button
+
     let clearScore = document.createElement("button");
     clearScore.textContent = "Clear Highscore";
     clearScore.className = "btn edit-btn";
@@ -262,7 +262,7 @@ function displayScores() {
     let data = localStorage.getItem("scores");
     data = data ? JSON.parse(data) : [];
 
-    // Sort score in descending order
+
     data.sort((a, b) => {
         return b.score - a.score;
     });
@@ -301,19 +301,19 @@ function viewScore() {
         alert("You can't view score while quiz in on-going");
         return;
     } else {
-        welcomeEl.style.display = "none";
+        initialEl.style.display = "none";
     }
 
 
     displayScores();
 }
-// This function reloads the page and go back to Home screen
+
 function goBackToHome() {
     location.reload();
 }
 
 
-// Event Listeners
+
 
 submitScore.addEventListener("click", saveScore);
 
